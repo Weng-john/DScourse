60a9060ac2e0403aa85769eb7be119698d03ebb3
Weng-john <59682713+Weng-john@users.noreply.github.com>
2023-03-21 04:26:22 +0800

    without form url

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+var formUrl=[];
+
+document.querySelector(".confirm").addEventListener("click",()=>{
+    let sel= document.getElementById("race").value;
+    //window.location.href= formUrl[sel-1];
+});
