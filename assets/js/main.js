7da52c4de3b713868aae0f0bf91a3432e04e3641
Weng-john <59682713+Weng-john@users.noreply.github.com>
2023-03-21 07:14:30 +0800

    update url

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,18 @@
-var formUrl=[];
+var formUrl=[
+    "https://forms.gle/FhQb41Bn8yHHpyaW9",
+    "https://forms.gle/3HTu36nTc7VcdAow7",
+    "https://forms.gle/ttSnEUJGUhPnhEAR9",
+    "https://forms.gle/3cNA9ydkbh6jaZ6Q7",
+    "https://forms.gle/q7JRAFfcDzvm2p8f8",
+    "https://forms.gle/eet3YEvXTtxBjDZP7",
+    "https://forms.gle/bHfriWwBba7pctUn8",
+    "https://forms.gle/PySHaiebGQfapx4i7",
+    "https://forms.gle/9EtpJBcggX4a53q38",
+    "https://forms.gle/MMW5KUjJy46WtUZe6",
+    "https://forms.gle/YckbXWVUzyTyzZHj9"
+];
 
 document.querySelector(".confirm").addEventListener("click",()=>{
     let sel= document.getElementById("race").value;
-    //window.location.href= formUrl[sel-1];
+    window.location.href= formUrl[sel-1];
 });
